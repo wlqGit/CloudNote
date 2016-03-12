a1d465b214c7dd234811ec1baafe00c4d4c51e5d
aristark <aristark@aristarkdeMacBook-Pro.local>
2016-03-12 20:26:56 +0800

    :)

---
+++ app/src/main/java/com/aristark/note/NoteAddActivity.java
@@ -0,0 +1,21 @@
+package com.aristark.note;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by aristark on 3/4/16.
+ */
+public class NoteAddActivity extends SingleFragmentActivity{
+    @Override
+    public Fragment creatFrament() {
+        return new NoteAddFragment();
+//        return new Fragment();
+    }
+
+    public static Intent newIntent(Context context){
+        Intent intent = new Intent(context,NoteAddActivity.class);
+        return intent;
+    }
+}
